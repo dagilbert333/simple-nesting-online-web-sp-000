d4d7dc4e48f684f969a1456fe0cf03fb97aa12d8
David Gilbert <dagilbert333@gmail.com>
2019-01-27 00:51:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -121,6 +121,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  programmer_hash[:dennis_ritchie][:languages][1] << "Assembly"
   programmer_hash
 end
